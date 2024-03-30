f148d1626576895842519c58563baab7c71290a6
Ilya Matsuk <g5129554@gmail.com>
2024-03-30 12:44:47 +0500

    add day 2 project

---
+++ 100 Days of Code The Complete Python Pro Bootcamp/Tasks/Day 2/project/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+number_of_people = int(input("How many people to split the bill? "))
+result = round((total_bill * ((100 + tip) / 100)) / number_of_people, 2)
+print(f"Each person should pay: ${result}")
